32a9afb807dabc2dde2098516a6f17141e99e328
Dillotex <brian@vertaasis.com>
2014-06-22 13:54:05 -0500

    Final Programming Assignment 2 File
    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -26,14 +26,33 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         
-        m <- NULL
+        ##***************************************************************************************
+        ##*  Initialize the m variable                                                          *
+        ##***************************************************************************************
+        m <- NULL        
+        ##***************************************************************************************
+        ##*   Define the "set" function to cache calculation results.                           *
+        ##***************************************************************************************
         set <- function(y) {
                 x <<- y
                 m <<- NULL
-        }
-        get <- function()x
-        setCacheMatrix <- function(solve) m <<- solve
-        getCacheMatrix <- function() m
+        }        
+        ##***************************************************************************************
+        ##*   Define the "get" function to retrieve cached calculation results.                 *
+        ##***************************************************************************************
+        get <- function()x        
+        ##***************************************************************************************
+        ##*   Define the "setCacheMatrix" function to save off a matrix in cache.               *
+        ##*   This cached matrix will also have had the inverse of the matrix derived.          *
+        ##***************************************************************************************
+        setCacheMatrix <- function(solve) m <<- solve        
+        ##***************************************************************************************
+        ##*   Define the "getCacheMatrix" function to retrieve the cached matrix.               *
+        ##***************************************************************************************
+        getCacheMatrix <- function() m        
+        ##***************************************************************************************
+        ##*   Return the functions as part of a list.                                           *
+        ##***************************************************************************************
         list(set = set, get = get,
              setCacheMatrix = setCacheMatrix,
              getCacheMatrix = getCacheMatrix)
@@ -47,20 +66,36 @@ makeCacheMatrix <- function(x = matrix()) {
 ##***********************************************************************************************
 
 cacheSolve <- function(x = matrix(), ...) {
+        ##         
+        ##***************************************************************************************
+        ##*   Return a matrix that is the inverse of 'x'                                        *
+        ##*   Refers to a method previously defined to retrieve the cached value                *
+        ##***************************************************************************************
         ## 
-        ## Return a matrix that is the inverse of 'x'
-        ## Refers to a method previously defined in makeCacheMatrix to retrieve the cached value 
+              m <- x$getCacheMatrix()        
+        ##***************************************************************************************
+        ##*   Check to see if m has been defined and if so, retrieve that value and return it.  *
+        ##***************************************************************************************
         ## 
-              m <- x$getCacheMatrix()
-              ## Check to see if m has been defined and if so, retrieve that value and return it.
               if(!is.null(m)) {
                       message("getting cached data")
                       return(solve(m))
               }
-              ## If no value was detected for m, do the calculation and return the inverted
-              ## matrix.
-              data <- x$get()
-              m <- solve(data, ...)
-              x$setCacheMatrix(m)
+        ##         
+        ##***************************************************************************************
+        ##*   If no value was detected for m, do the calculation and return the inverted matrix.*
+        ##***************************************************************************************
+              data <- x$get()        
+        ##***************************************************************************************
+        ##*   Calculate the inverse of the provided matrix.                                     *
+        ##***************************************************************************************
+              m <- solve(data, ...)        
+        ##***************************************************************************************
+        ##*   Store off the calculated inverse matrix in cache.                                 *
+        ##***************************************************************************************
+              x$setCacheMatrix(m)        
+        ##***************************************************************************************
+        ##*   Return the calculated inverse matrix                                              *
+        ##***************************************************************************************
               m
 }
